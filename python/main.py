d38acb9a53f97f6bce6ec34e65d37f8dc64889dd
jon cosgrove <jonmcosgrove@gmail.com>
2018-02-10 09:56:18 -0800

    Logging configured in main via Src/logging.cong

--- python/main.py
+++ python/main.py
@@ -5,67 +5,28 @@ Created on Sat Jan 20 11:34:30 2018
 @author: pi
 """
 
-import serial
+#import serial
+import logging
+import logging.config
 import pandas as pd
+from Src import GPS_VK172 as vk
+import datetime as dt
 
 
-def get_position(serial_port, data_container):
-    '''Read GPS position (message GPGLL) from VK-172
-        Serial_port = the serial port of the USB GPS receiver
-            ex: ser = serial.Serial('/dev/ttyACM0')
-        Data_container = pandas data frame used to store test run data
-    '''
-# port is a serial object for GPS
-# read data
-# format into df
-# return data frame
-    print('called')
-    d = {}
-    readComplete = False
-    while readComplete == False:
-        line = str(serial_port.readline())
-        [msg, data] = line[2:].split(',', 1)
-#       for debug
-        print('Received ' + msg)
-        if msg == '$GPGLL':
-# store into local variables
-            [gps_lat, gps_latDir, gps_lon, gps_lonDir, gps_time,
-             gps_valid, gps_cksum] = data.split(',', 6)
-            for i in ('gps_lat', 'gps_latDir', 'gps_lon', 'gps_lonDir',
-                      'gps_time', 'gps_valid'):
-# fill dict d with keys for data fields and cooresponding data
-                d[i] = locals()[i]
-                dd = pd.DataFrame([d], columns=d.keys())
-            print(dd)
-            return store(dd, data_container)
-            break
+# setup logging
+filename = dt.datetime.now().strftime('%Y%m%d_%H%M%S%p') + '_main.log'
+logging.config.fileConfig('Src/logging.conf',
+                          defaults={'logfilename': 'Log/'+filename})
+logger = logging.getLogger('main')
+logger.info('Begin log ' + dt.datetime.now().strftime('%Y%m%d_%H%M%S%p'))
 
+df = vk.get_position()
 
-def store(data_dict, main_df):
-    ''' Store data in larger dataframe during collection, can be dictionary'''
-    for i in data_dict.keys():
-        if i not in list(main_df):
-            print('Data  key "' + i + '" is not in main dataframe')
-            return main_df
+for i in range(10):
+    df = vk.get_position()
+    logger.warning('position aquired.')
 
-    main_df = main_df.append(data_dict)
-
-    return main_df
-
-
-columns = ['gps_lat', 'gps_latDir', 'gps_lon', 'gps_lonDir', 'gps_time',
-           'gps_valid']
-
-
-df = pd.DataFrame(columns=columns)
-ser = serial.Serial('/dev/ttyACM0')
-df = get_position(ser, df)
-
-for i in range(1):
-    get_position(ser, df)
-
-df = get_position(ser, df)
 print(df)
+logger.warning('hello')
 
 r = {'check': 99, 'gps_valid': 'B'}
-store(r, df)
