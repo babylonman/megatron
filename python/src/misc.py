091e879ac5845e63d668e289ad7210b3cbdde534
Jon Cosgrove <joncosgrove@Jons-MacBook-Pro.local>
2018-03-24 20:10:17 -0700

    new function get_coord(), xy pairs from feature collection

--- python/src/misc.py
+++ python/src/misc.py
@@ -11,6 +11,34 @@ import logging
 import datetime as dt
 
 
+def get_coord(feature_collection):
+    ''' get x y coordinate pairs from geojson feature collection object
+    Parameters
+    ----------
+    feature_collection : geojson feature collection object
+
+    Tests
+    -----
+    >>> import geojson
+    >>> import os
+    >>> p = os.path.dirname(os.getcwd())
+    >>> waypoint_file = p + '/test/path_plan/data/Baseball_feat_coll_dump.json'
+    >>> waypoints = geo.load(open(waypoint_file, 'r'))
+    >>> get_coord(waypoints)
+    ([37.3779926, 37.3778115, 37.3777806, 37.3779436], [-122.0419519, -122.0420216, -122.0417949, -122.0417266])
+
+    Return
+    ------
+    [x, y] list which preserves order found in feature_collection
+    '''
+    x, y = [], []
+    for i in feature_collection.features:
+        x.append(i.geometry.coordinates[0])
+        y.append(i.geometry.coordinates[1])
+
+    return x, y
+
+
 def configure_logging(path='/home/pi/megatron/python/Log', filemode='w',
                       level=logging.DEBUG):
     '''Setup logging given a path and event serverity level
@@ -28,9 +56,12 @@ def configure_logging(path='/home/pi/megatron/python/Log', filemode='w',
     '''
 
     filename = 'Log_' + dt.datetime.now().strftime('%Y%m%d_%H%M%S%p') \
-                + '.log'
+               + '.log'
     print(filename)
     logging.basicConfig(filename=filename, level=level)
     logging.info('Begin')
 
-configure_logging()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
